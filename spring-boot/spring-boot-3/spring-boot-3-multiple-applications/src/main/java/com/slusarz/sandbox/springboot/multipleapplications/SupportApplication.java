3ab59a70f45beab38b79859e36ced245f27e4fae
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-10 13:46:45 +0200

    SB: multiple spring applications

--- spring-boot/spring-boot-3/spring-boot-3-multiple-applications/src/main/java/com/slusarz/sandbox/springboot/multipleapplications/SupportApplication.java
+++ spring-boot/spring-boot-3/spring-boot-3-multiple-applications/src/main/java/com/slusarz/sandbox/springboot/multipleapplications/SupportApplication.java
@@ -1,13 +1,18 @@
 package com.slusarz.sandbox.springboot.multipleapplications;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SupportApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SupportApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(SupportApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args)
+                .close();
     }
 
 }
