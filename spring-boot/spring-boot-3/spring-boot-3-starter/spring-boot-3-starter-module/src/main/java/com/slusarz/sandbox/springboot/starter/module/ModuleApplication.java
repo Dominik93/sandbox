4849b6308b845612decfa1fd6db657191323905f
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-21 17:30:02 +0200

    SB: starter remove unnecessary import

--- spring-boot/spring-boot-3/spring-boot-3-starter/spring-boot-3-starter-module/src/main/java/com/slusarz/sandbox/springboot/starter/module/ModuleApplication.java
+++ spring-boot/spring-boot-3/spring-boot-3-starter/spring-boot-3-starter-module/src/main/java/com/slusarz/sandbox/springboot/starter/module/ModuleApplication.java
@@ -4,11 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootApplication
-@Import(ModuleConfiguration.class)
 public class ModuleApplication {
 
     public static void main(String[] args) {
