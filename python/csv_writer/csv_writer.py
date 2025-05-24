6256c2387422f366405392331b984113f145e16c
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-24 13:24:01 +0200

    csv writer, sanitize

--- python/csv_writer/csv_writer.py
+++ python/csv_writer/csv_writer.py
@@ -1,6 +1,29 @@
 import datetime
-import time
+import re
 
+emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002500-\U00002BEF"  # chinese char
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           u"\U0001f926-\U0001f937"
+                           u"\U00010000-\U0010ffff"
+                           u"\u2640-\u2642"
+                           u"\u2600-\u2B55"
+                           u"\u200d"
+                           u"\u23cf"
+                           u"\u23e9"
+                           u"\u231a"
+                           u"\ufe0f"  # dingbats
+                           u"\u3030"
+                           "]+", flags=re.UNICODE)
+
+
+def _sanitize(row):
+    return emoji_pattern.sub(r'', row).replace("\xb2", "")
 
 def _prepare_header(content):
     header = []
@@ -22,5 +45,5 @@ def write(file: str, content: list[dict], separator: str = ";", headers: list[st
     with open(file.replace("{timestamp}", datetime.datetime.now().strftime("%H-%M-%S")), "w", encoding="utf-8") as f:
         f.write(separator.join(headers_row) + "\n")
         for item in content:
-            row = _prepare_row(headers_row, item)
+            row = _sanitize(_prepare_row(headers_row, item))
             f.write(separator.join(row) + "\n")
