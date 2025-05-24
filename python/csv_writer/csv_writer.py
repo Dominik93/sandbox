dc81319fc8948cc635eced741e0e6633210b6a21
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-24 12:36:27 +0200

    csv writer, add timestamp

--- python/csv_writer/csv_writer.py
+++ python/csv_writer/csv_writer.py
@@ -1,3 +1,7 @@
+import datetime
+import time
+
+
 def _prepare_header(content):
     header = []
     for key in content[0]:
@@ -15,7 +19,7 @@ def _prepare_row(headers_row, item):
 
 def write(file: str, content: list[dict], separator: str = ";", headers: list[str] = None):
     headers_row = headers if headers is not None else _prepare_header(content)
-    with open(file, "w", encoding="utf-8") as f:
+    with open(file.replace("{timestamp}", datetime.datetime.now().strftime("%H-%M-%S")), "w", encoding="utf-8") as f:
         f.write(separator.join(headers_row) + "\n")
         for item in content:
             row = _prepare_row(headers_row, item)
--- python/csv_writer/main.py
+++ python/csv_writer/main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
                {"col_1": "val.2.1", "col_2": None}]
 
     write("test.csv", content)
+    write("test-{timestamp}.csv", content)
     write("test2.csv", content, headers=["col_1"])
     write("test3.csv", content, headers=["col_1", "col_2"])
     write("test4.csv", content, separator="-")
