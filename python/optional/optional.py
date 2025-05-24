8830797303acd67ab54eca9b53d2bcca430e262c
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-24 09:39:41 +0200

    optional

--- python/optional/optional.py
+++ python/optional/optional.py
@@ -12,22 +12,32 @@ class Optional:
         self.value = value
 
     def map(self, mapper):
-        if self.value is None:
+        if self.is_empty():
             return Optional(None)
         return Optional(mapper(self.value))
 
     def get(self):
-        if self.value is None:
+        if self.is_empty():
             raise Exception("Value is None")
         return self.value
 
     def or_get(self, other):
-        return other if self.value is None else self.value
+        return other if self.is_empty() else self.value
 
     def or_else_get(self, other_provider):
-        return other_provider() if self.value is None else self.value
+        return other_provider() if self.is_empty() else self.value
 
     def or_else_throw(self):
-        if self.value is None:
+        if self.is_empty():
             raise Exception("Value is None")
         return self.value
+
+    def if_present(self, processor):
+        if self.is_present():
+            processor(self.value)
+
+    def is_present(self):
+        return self.value is not None
+
+    def is_empty(self):
+        return self.value is None
