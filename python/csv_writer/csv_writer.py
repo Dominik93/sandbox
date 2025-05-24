d13cf478784dfa798570e32e1df63f8f99f27956
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-24 08:37:25 +0200

    dict csv writer

---
+++ python/csv_writer/csv_writer.py
@@ -0,0 +1,22 @@
+def _prepare_header(content):
+    header = []
+    for key in content[0]:
+        header.append(key)
+    return header
+
+
+def _prepare_row(headers_row, item):
+    row = []
+    for key in item:
+        if key in headers_row:
+            row.append(item[key])
+    return row
+
+
+def write(file: str, separator: str, content: list[dict], headers: list[str] = None):
+    headers_row = headers if headers is not None else _prepare_header(content)
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(separator.join(headers_row) + "\n")
+        for item in content:
+            row = _prepare_row(headers_row, item)
+            f.write(separator.join(row) + "\n")
