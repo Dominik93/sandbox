833b8db86e68591f97f9ca3885766f1c966d6270
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-25 19:35:51 +0200

    logger, reformat messages

--- python/logger/logger.py
+++ python/logger/logger.py
@@ -5,8 +5,7 @@ from enum import Enum
 class Level(Enum):
     INFO = 1
     DEBUG = 2
-    TRACE = 3
-    OFF = 4
+    OFF = 3
 
 
 def log(level=Level.INFO,
@@ -15,14 +14,14 @@ def log(level=Level.INFO,
     def log_decorator(func):
         def log_wrapper(*args, **kwargs):
             start = time.time_ns()
-            log_args = _get_log(level, [Level.INFO, Level.OFF], lambda: args)
-            _print(level, func.__name__, start_message, lambda x: x.format(name=func.__name__, args=log_args))
+            _print(level, func.__name__, start_message,
+                   lambda x: x.format(name=func.__name__, args=args))
             result = func(*args, **kwargs)
 
             duration = _get_duration(start)
-            log_result = _get_log(level, [Level.INFO, Level.OFF], lambda: f" returned: {str(result)}")
             _print(level, func.__name__, end_message,
-                   lambda x: x.format(name=func.__name__, args=log_args, result=log_result, duration=duration))
+                   lambda x: x.format(name=func.__name__, args=args, result=f" returned: {str(result)}",
+                                      duration=duration))
             return result
 
         return log_wrapper
@@ -41,6 +40,9 @@ def _convert_to_ms(ns):
 def _print(level, name, log_format, content_provider):
     if log_format is None:
         return
+    if level.value < Level.DEBUG.value:
+        log_format = (log_format.replace("{args}", "").replace("{args} ", "")
+                      .replace("{result} ", "").replace("{result}", ""))
     if level != Level.OFF:
         _print_console(level, name, log_format, content_provider)
         _print_file(level, name, log_format, content_provider)
