ea9e6ea2ba3c8ba884fcdb0815f91da43849f2b7
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-25 10:21:20 +0200

    configuration reader, get method prevents missing config

--- python/configuration_reader/configuration_reader.py
+++ python/configuration_reader/configuration_reader.py
@@ -2,9 +2,23 @@ import json
 
 
 def read_configuration(name):
-    return read(name)
+    with open(name + ".json", 'r', encoding="utf-8") as file:
+        return Config(json.load(file))
 
 
-def read(file):
-    with open(file + ".json", 'r', encoding="utf-8") as file:
-        return json.load(file)
+class Config:
+
+    def __init__(self, config):
+        self.config = config
+
+    def get(self, path: str, default: any = '') -> dict | list | str | int:
+        return self._get_or_default(path.split("."), default)
+
+    def _get_or_default(self, property_names: list, default: any = '') -> dict | list | str | int:
+        result = self.config
+        for property_name in property_names:
+            if result is not None and property_name in result:
+                result = result[property_name]
+            else:
+                return default
+        return result
---
+++ python/configuration_reader/main.py
@@ -0,0 +1,9 @@
+from configuration_reader import read_configuration
+
+if __name__ == "__main__":
+    print(read_configuration('config').get("item.value", "0"))
+    print(read_configuration('config').get("item.other_value", "0"))
+    print(read_configuration('config').get("item.list", []))
+    print(read_configuration('config').get("item.other_list", ["1"]))
+    print(read_configuration('config').get("value", {"sample": "0"}))
+    print(read_configuration('config').get("other_value", {"sample": "0"}))
