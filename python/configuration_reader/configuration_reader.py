9718f427389dc120a0e8a82c6ce2e2ba24d4f4c0
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-05-25 10:30:59 +0200

    configuration reader, cache configs

--- python/configuration_reader/configuration_reader.py
+++ python/configuration_reader/configuration_reader.py
@@ -1,9 +1,18 @@
 import json
 
+__cached_configs = {}
 
-def read_configuration(name: str, factory_provider=lambda x: Config(x)):
+
+def read_configuration(name: str, factory_provider=lambda x: Config(x), use_cache=False):
+    global __cached_configs
+    if use_cache and name in __cached_configs:
+        return __cached_configs[name]
+
     with open(name + ".json", 'r', encoding="utf-8") as file:
-        return factory_provider(json.load(file))
+        config = factory_provider(json.load(file))
+        if use_cache:
+            __cached_configs[name] = config
+        return config
 
 
 class Config:
--- python/configuration_reader/main.py
+++ python/configuration_reader/main.py
@@ -7,3 +7,6 @@ if __name__ == "__main__":
     print(read_configuration('config').get("item.other_list", ["1"]))
     print(read_configuration('config').get("value", {"sample": "0"}))
     print(read_configuration('config').get("other_value", {"sample": "0"}))
+
+    print(read_configuration('config', use_cache=True).get("item.value", "0"))
+    print(read_configuration('config', use_cache=True).get("item.value", "0"))
