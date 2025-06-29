874145aa60d903a87dc5c5ef845714380bd3f2a5
Dominik Ślusarz <slusarz.dominik@gmail.com>
2025-06-29 11:01:48 +0200

    lists - partition

--- python/utils/lists.py
+++ python/utils/lists.py
@@ -1,6 +1,12 @@
 from typing import Callable
 
-from optional.optional import Optional, empty, of
+from optional.optional import Optional, of, empty
+
+
+def partition(items, size):
+    if len(items) < size:
+        return [items]
+    return list(_partition(items, size))
 
 
 def find_item(items: list[any], predicate: Callable) -> Optional:
@@ -8,3 +14,8 @@ def find_item(items: list[any], predicate: Callable) -> Optional:
         if predicate(item):
             return of(item)
     return empty()
+
+
+def _partition(items, size):
+    for i in range(0, len(items) // size):
+        yield items[i:: size]
--- python/utils/lists_test.py
+++ python/utils/lists_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lists import find_item
+from lists import find_item, partition
 
 
 class ListsTestCase(unittest.TestCase):
@@ -9,6 +9,24 @@ class ListsTestCase(unittest.TestCase):
         items = ["1", "2", "3"]
         item = find_item(items, lambda x: x == "2")
         self.assertEqual("2", item.get())
+
+    def test_should_partition_array(self):
+        items = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        partition_items = partition(items, 3)
+        self.assertEqual([["1", "4", "7"],
+                          ["2", "5", "8"],
+                          ["3", "6", "9"]], partition_items)
+    def test_should_partition_array_when_size_is_not_divider_of_items_length(self):
+        items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        partition_items = partition(items, 3)
+        self.assertEqual([["1", "4", "7", "10"],
+                          ["2", "5", "8"],
+                          ["3", "6", "9"]], partition_items)
+
+    def test_should_partition_array_when_size_is_smaller_than_items_length(self):
+        items = ["1", "2", "3"]
+        partition_items = partition(items, 5)
+        self.assertEqual([["1", "2", "3"]], partition_items)
 
 
 if __name__ == '__main__':
